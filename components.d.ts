5bbd7f527012f530b6e8bf4d3e721a14571ebf67
xiaoxunyao <huchengyea@163.com>
2022-04-13 09:52:21 +0800

    chore: init

--- components.d.ts
+++ components.d.ts
@@ -4,8 +4,6 @@
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    Counter: typeof import('./src/components/Counter.vue')['default']
-    Footer: typeof import('./src/components/Footer.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
